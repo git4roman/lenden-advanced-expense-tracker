50cf2d1442eea463e15d8416517ba4ef0e11cfd9
Roman Khatri <romanforgit@gmail.com>
2026-02-09 01:00:39 +0545

    fix(groups): removed background color

--- Lenden.Mobile/app/(tabs)/(groups)/index.tsx
+++ Lenden.Mobile/app/(tabs)/(groups)/index.tsx
@@ -113,130 +113,144 @@ const GroupScreen = () => {
       </View> */}
       <ScrollView
         style={{
-          backgroundColor: Colors.neutral[900],
+          //   backgroundColor: Colors.neutral[900],
           paddingHorizontal: 8,
           paddingTop: 16,
           borderRadius: 8 + 8,
           flex: 1,
         }}
-        contentContainerStyle={{ gap: 16 }}
+        contentContainerStyle={{ gap: 8 }}
       >
-        {groupsData.map((group) => (
-          <Pressable
-            key={group.id}
-            style={{
-              flexDirection: "row",
-              gap: 8,
-              backgroundColor: Colors.neutral[800],
-              borderWidth: 1,
-              borderRadius: 16,
-              borderColor: "transparent",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              padding: 12,
-            }}
-            onPress={() => {
-              console.log("Card is pressed");
-            }}
-          >
-            <View
-              style={{
-                width: 50,
-                height: 50,
-                borderWidth: 1,
-                borderColor: Colors.accent[600],
-                borderRadius: "50%",
-                overflow: "hidden",
-              }}
-            >
-              <Image
-                source={{ uri: group.image }}
-                resizeMode="contain"
-                style={{ width: "100%", height: "100%" }}
-              />
-            </View>
-            <View
+        {groupsData.map((group, index) => (
+          <>
+            <Pressable
+              key={group.id}
               style={{
                 flexDirection: "row",
-                justifyContent: "space-between",
+                gap: 16,
+                backgroundColor: Colors.neutral[800],
+                // borderWidth: 1,
+                borderRadius: 16,
+                // borderColor: "transparent",
+                justifyContent: "flex-start",
                 alignItems: "center",
-                // gap: 16,
-                flex: 1,
+                paddingVertical: 12,
+                paddingRight: 12,
+              }}
+              onPress={() => {
+                console.log("Card is pressed");
               }}
             >
-              <View style={{ gap: 4 }}>
-                <CText weight="semibold" size="md" color="neutral" shade={300}>
-                  {group.label}
-                </CText>
-                <View style={{ flexDirection: "row", gap: 0 }}>
-                  <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    {group.members.slice(0, 2).map((member, index) => (
-                      <View
-                        key={index}
-                        style={{
-                          borderColor: Colors.accent[800],
-                          width: 28,
-                          height: 28,
-                          borderWidth: 2,
-                          borderRadius: 25,
-                          overflow: "hidden",
-                          marginLeft: index === 0 ? 0 : -8,
-                        }}
-                      >
-                        <Image
-                          source={{ uri: member }}
-                          style={{ width: "100%", height: "100%" }}
-                        />
-                      </View>
-                    ))}
-
-                    {group.members.length > 2 && (
-                      <ImageBackground
-                        source={{ uri: group.members[2] }}
-                        style={{
-                          width: 28,
-                          height: 28,
-                          borderRadius: 25,
-                          backgroundColor: Colors.neutral[700],
-                          justifyContent: "center",
-                          alignItems: "center",
-                          marginLeft: -8,
-                          borderWidth: 2,
-                          borderColor: Colors.accent[800],
-                          overflow: "hidden",
-                        }}
-                      >
-                        <CText
-                          size="xs"
-                          weight="semibold"
-                          color="neutral"
-                          shade={400}
-                        >
-                          +{group.members.length - 2}
-                        </CText>
-                      </ImageBackground>
-                    )}
-                  </View>
-
-                  <View style={{ justifyContent: "flex-end" }}>
-                    <CText
-                      size="md"
-                      color="accent"
-                      shade={500}
-                      style={{ marginLeft: -2 }}
+              <View
+                style={{
+                  width: 75,
+                  height: 55,
+                  borderWidth: 1,
+                  borderColor: Colors.accent[600],
+                  borderRadius: 8,
+                  overflow: "hidden",
+                }}
+              >
+                <Image
+                  source={{ uri: group.image }}
+                  resizeMode="cover"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  // gap: 16,
+                  flex: 1,
+                }}
+              >
+                <View style={{ gap: 4 }}>
+                  <CText
+                    weight="semibold"
+                    size="md"
+                    color="neutral"
+                    shade={300}
+                  >
+                    {group.label}
+                  </CText>
+                  <View style={{ flexDirection: "row", gap: 0 }}>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
                     >
-                      ...
-                    </CText>
+                      {group.members.slice(0, 3).map((member, index) => (
+                        <View
+                          key={index}
+                          style={{
+                            borderColor: Colors.accent[800],
+                            width: 20,
+                            height: 20,
+                            borderWidth: 2,
+                            borderRadius: 25,
+                            overflow: "hidden",
+                            marginLeft: index === 0 ? 0 : -6,
+                          }}
+                        >
+                          <Image
+                            source={{ uri: member }}
+                            style={{ width: "100%", height: "100%" }}
+                          />
+                        </View>
+                      ))}
+
+                      {group.members.length > 3 && (
+                        <ImageBackground
+                          source={{ uri: group.members[2] }}
+                          blurRadius={15}
+                          style={{
+                            width: 20,
+                            height: 20,
+                            borderRadius: 25,
+                            backgroundColor: Colors.neutral[700],
+                            justifyContent: "center",
+                            alignItems: "center",
+                            marginLeft: -6,
+                            borderWidth: 2,
+                            borderColor: Colors.accent[800],
+                            overflow: "hidden",
+                            opacity: 0.6,
+                          }}
+                        >
+                          <CText
+                            size="xs"
+                            weight="semibold"
+                            color="neutral"
+                            shade={50}
+                          >
+                            +{group.members.length - 3}
+                          </CText>
+                        </ImageBackground>
+                      )}
+                    </View>
                   </View>
                 </View>
+                <View style={{}}>
+                  <Pressable onPress={() => console.log("first")} style={{}}>
+                    <ArrowRight2 size="24" color={Colors.accent[500]} />
+                  </Pressable>
+                </View>
               </View>
-              <View style={{}}>
-                <Pressable onPress={() => console.log("first")} style={{}}>
-                  <ArrowRight2 size="24" color={Colors.accent[500]} />
-                </Pressable>
-              </View>
-            </View>
-          </Pressable>
+            </Pressable>
+            {groupsData.length - 1 !== index && (
+              <View
+                style={{
+                  width: "100%",
+                  height: 1,
+                  backgroundColor: Colors.neutral[700],
+                  alignSelf: "center",
+                }}
+              />
+            )}
+          </>
         ))}
       </ScrollView>
     </SafeAreaView>
