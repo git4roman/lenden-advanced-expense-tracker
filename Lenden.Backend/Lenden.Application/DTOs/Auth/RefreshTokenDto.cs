9a81324bcd1fb1057fb3baec19cff1c5edeedbd8
Roman Khatri <romanforgit@gmail.com>
2026-02-16 19:03:22 +0545

    refresh token dto

---
+++ Lenden.Backend/Lenden.Application/DTOs/Auth/RefreshTokenDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Lenden.Application.DTOs;
+
+public record RefreshTokenRequest(string RefreshToken, string DeviceInfo, string IpAddress);
+
+public record RefreshTokenResponse(string AccessToken, string RefreshToken);
