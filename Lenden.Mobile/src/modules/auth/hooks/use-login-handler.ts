92b5660e20cd72f87f4e30a042a9ba2d6caee189
Roman Khatri <romanforgit@gmail.com>
2026-05-19 15:25:52 +0545

    HCanged

--- Lenden.Mobile/src/modules/auth/hooks/use-login-handler.ts
+++ Lenden.Mobile/src/modules/auth/hooks/use-login-handler.ts
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { router } from "expo-router";
-import Toast from "react-native-toast-message";
 import { useLoginMutation } from "@/src/shared/store/apiSlices/auth-slice.api";
+import { router } from "expo-router";
+import { useState } from "react";
+import Toast from "react-native-toast-message";
 
 export const useLoginHandler = () => {
   const [email, setEmail] = useState("admin@admin.com");
@@ -19,12 +19,27 @@ export const useLoginHandler = () => {
       Toast.show({ type: "success", text1: "Login Successful" });
       router.replace("/(tabs)/home");
     } catch (error: any) {
-      console.log("Error from login", error);
-      Toast.show({
-        type: "error",
-        text1: "Login Failed",
-        text2: error?.data?.message ?? "Invalid credentials",
-      });
+      console.log(error);
+
+      switch (error?.status) {
+        case "FETCH_ERROR":
+          return;
+
+        case 401:
+          Toast.show({
+            type: "error",
+            text1: "Unauthorized",
+            text2: "Invalid credentials",
+          });
+          return;
+
+        default:
+          Toast.show({
+            type: "error",
+            text1: "Error",
+            text2: error?.data?.message ?? "Something went wrong",
+          });
+      }
     }
   };
 
--- Lenden.Mobile/src/shared/store/apiSlices/apiClient.ts
+++ Lenden.Mobile/src/shared/store/apiSlices/apiClient.ts
@@ -1,35 +1,62 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API_CONFIG } from "@/api.config";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Toast from "react-native-toast-message";
 import { RootState } from "../store";
 
 const BASE_URL = `${API_CONFIG.BASE_URL}/api/v1`;
-// const BASE_URL = "https://localhost:44361/api/v1";
+
+const rawBaseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+
+  prepareHeaders: (headers, { getState }) => {
+    const state = getState() as RootState;
+    const token = state.auth.accessToken;
+
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const baseQuery: typeof rawBaseQuery = async (args, api, extraOptions) => {
+  const result = await rawBaseQuery(args, api, extraOptions);
+
+  if (result.error) {
+    const error = result.error;
+
+    if (error.status === "FETCH_ERROR") {
+      Toast.show({
+        type: "error",
+        text1: "Network Error",
+        text2: "Please check your internet connection",
+      });
+    }
+
+    if (error.status === 500) {
+      Toast.show({
+        type: "error",
+        text1: "Server Error",
+        text2: "Something went wrong",
+      });
+    }
+
+    if (error.status === 401) {
+      Toast.show({
+        type: "error",
+        text1: "Unauthorized",
+        text2: "Please login again",
+      });
+    }
+  }
+
+  return result;
+};
 
 export const api = createApi({
   reducerPath: "api",
+  baseQuery,
   tagTypes: ["Group", "User", "Expense", "Friend"],
-  baseQuery: async (args, apiApi, extraOptions) => {
-    const rawBaseQuery = fetchBaseQuery({
-      baseUrl: BASE_URL,
-      prepareHeaders: async (headers) => {
-        const state = apiApi.getState() as RootState;
-        const token = state.auth.accessToken;
-        if (token) {
-          headers.set("Authorization", `Bearer ${token}`);
-        }
-        return headers;
-      },
-    });
-
-    const result = await rawBaseQuery(args, apiApi, extraOptions);
-    // if (typeof args === "string") {
-    //   console.log("RTK Query", args, JSON.stringify(result));
-    // } else {
-    //   console.log("RTK Query", args?.method, args?.url, JSON.stringify(result));
-    // }
-    return result;
-  },
-
   endpoints: () => ({}),
 });
